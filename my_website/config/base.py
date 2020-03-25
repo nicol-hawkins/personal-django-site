1c46edd5fcfd2cf5befb4d43064ec09f17177404
NicolCaresse <nicolchawkins@gmail.com>
2020-03-24 21:01:05 -0700

    static

--- my_website/config/base.py
+++ my_website/config/base.py
@@ -114,11 +114,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'my_website/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # We also needed to add this to specify where the downloads go
