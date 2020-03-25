a28f31c8b89e279f31437cbfa6518c3470c0cfdb
NicolCaresse <nicolchawkins@gmail.com>
2020-03-24 20:45:52 -0700

    static

--- my_website/config/base.py
+++ my_website/config/base.py
@@ -118,7 +118,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # We also needed to add this to specify where the downloads go
